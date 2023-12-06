56282e27e6793fae2d81748ae7b30decd01b9d7b
Infinity Developemnt <152995292+InfinityDevelopmentfivem@users.noreply.github.com>
2023-12-06 17:07:14 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 game 'gta5'
 
 author 'Infinity Development'
-description 'Player automatic bearth grow. A Free Script of Infinity Developent.'
+description 'Player automatic beard grow. A Free Script of Infinity Developent.'
 version '1.0.0'
 
 client_scripts {
