c72775f3aaafbbfb6b3364741c265939a7daf4a7
Infinity Developemnt <152995292+InfinityDevelopmentfivem@users.noreply.github.com>
2023-12-06 17:36:35 +0100

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,6 +1,6 @@
 Citizen.CreateThread(function() 
     while true do
-        Citizen.Wait(10000) 
+        Citizen.Wait(35* 60000) 
         TriggerEvent('skinchanger:getSkin', function (skinData)
             if skinData ~= nil then
                 if skinData['beard_2'] > 0 and skinData['beard_2'] < 10 then
